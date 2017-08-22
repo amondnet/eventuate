4192b2744a980e011611649bbfbe5a1fdf157990
amond <amond@leafmill.com>
2017-08-22 17:01:02 +0900

    eventuate

--- eventuate-java/src/test/java/SaveTest.java
+++ eventuate-java/src/test/java/SaveTest.java
@@ -1,17 +1,42 @@
 import co.test.Asdf;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.msemys.esjc.CatchUpSubscription;
+import com.github.msemys.esjc.CatchUpSubscriptionListener;
+import com.github.msemys.esjc.CatchUpSubscriptionSettings;
+import com.github.msemys.esjc.EventData;
 import com.github.msemys.esjc.EventStore;
 import com.github.msemys.esjc.EventStoreBuilder;
+import com.github.msemys.esjc.EventStoreException;
+import com.github.msemys.esjc.ExpectedVersion;
+import com.github.msemys.esjc.ResolvedEvent;
+import com.github.msemys.esjc.StreamMetadata;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+import net.amond.eventuate.getevenstore.GetEventStoreEventMapper;
 import net.amond.eventuate.getevenstore.GetEventStoreRepository;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
  * @author Minsu Lee
  */
 public class SaveTest {
+
+  private ObjectMapper objectMapper;
+
+  @BeforeClass
+  public void setUp() {
+    this.objectMapper = new ObjectMapper();
+    objectMapper.findAndRegisterModules();
+  }
+
   @Test
   public void saveTest() throws ExecutionException, InterruptedException, JsonProcessingException {
 
@@ -39,13 +64,5 @@ public class SaveTest {
         .singleNodeAddress("127.0.0.1", 1113)
         .userCredentials("admin", "changeit")
         .build();
-  }
-
-  @Test
-  public void test() {
-
-    System.out.println(Asdf.class.getName());
-    System.out.println(Asdf.class.getTypeName());
-    System.out.println(Asdf.class.getSimpleName());
   }
 }
